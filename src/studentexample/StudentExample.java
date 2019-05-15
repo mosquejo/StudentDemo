e6044f5550467898731e4c794c776bf210ca8a71
mosquejo <mosquejo@sheridancollege.ca>
2019-05-15 11:47:53 -0400

    Add Student Andres

--- src/studentexample/StudentExample.java
+++ src/studentexample/StudentExample.java
@@ -12,14 +12,16 @@ public class StudentExample {
         Student student3 = new Student("Juanita",3,9991);
         Student student4 = new Student("Martin",6,7834);
         Student student5 = new Student("Andrea",41,65434);
+        Student student6 = new Student("Andres",41,34563);
         
-        Student[] studentArray = new Student[5];
+        Student[] studentArray = new Student[6];
         
         studentArray[0] = student1;
         studentArray[1] = student2;
         studentArray[2] = student3;
         studentArray[3] = student4;
         studentArray[4] = student5;
+        studentArray[5] = student6;
         
         for (Student student:studentArray) {
             System.out.println(student.toString());
